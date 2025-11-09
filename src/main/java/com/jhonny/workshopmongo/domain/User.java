af174cb25142268e51283cf7f517b04bd25691d0
Jhonny515 <jonatascemporcentojesus@gmail.com>
2025-11-09 18:41:13 -0300

    Referenciando os posts do usuário

--- src/main/java/com/jhonny/workshopmongo/domain/User.java
+++ src/main/java/com/jhonny/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.jhonny.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -12,7 +15,10 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
+    // CONSTRUCTORS
     public User() {
     }
     public User(String id, String name, String email) {
@@ -20,7 +26,9 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
     }
+    // CONSTRUCTORS
 
+    // GETTERS AND SETTERS
     public String getId() {
         return id;
     }
@@ -44,6 +52,15 @@ public class User implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+    // GETTERS AND SETTERS
 
     @Override
     public boolean equals(Object o) {
