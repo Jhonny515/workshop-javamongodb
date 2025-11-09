9964f327c197adee9445009fea740ef50c4a7abe
Jhonny515 <jonatascemporcentojesus@gmail.com>
2025-11-09 18:32:13 -0300

    Projeção dos dados do Author com DTO

---
+++ src/main/java/com/jhonny/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,41 @@
+package com.jhonny.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.jhonny.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public AuthorDTO(User objUser) {
+        this.id = objUser.getId();
+        this.name = objUser.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
