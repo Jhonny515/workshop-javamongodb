0bf2a4d52d7760616f31147821197caae2663ebe
Jhonny515 <jonatascemporcentojesus@gmail.com>
2025-11-09 20:07:43 -0300

    Consulta com vários critérios

--- src/main/java/com/jhonny/workshopmongo/resources/util/URL.java
+++ src/main/java/com/jhonny/workshopmongo/resources/util/URL.java
@@ -1,6 +1,10 @@
 package com.jhonny.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class URL {
     public static String decodeParam(String text) {
@@ -8,6 +12,16 @@ public class URL {
             return java.net.URLDecoder.decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return "";
+        }
+    }
+    public static Date convertDate(String textDate, Date defaultDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultDate;
         }
     }
 }
