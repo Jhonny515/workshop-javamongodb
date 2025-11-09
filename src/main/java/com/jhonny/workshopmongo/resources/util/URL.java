f16e963c1764bc786d7bc42d20b8690fd3079ed2
Jhonny515 <jonatascemporcentojesus@gmail.com>
2025-11-09 19:31:41 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/jhonny/workshopmongo/resources/util/URL.java
@@ -0,0 +1,13 @@
+package com.jhonny.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+
+public class URL {
+    public static String decodeParam(String text) {
+        try {
+            return java.net.URLDecoder.decode(text, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+}
